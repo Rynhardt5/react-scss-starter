f50326320bf908af4afc5c3373edb5a5659d46b1
Rynhardt Smith <rynhardt.smith@gmail.com>
2018-11-09 14:20:51 +1100

    add npm deps

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div>React Starter with scss scaffold</div>;
+  return (
+    <div>
+      React Starter (create-react-app) with scss scaffold +
+      react-router-dom/redux/redux-form deps
+    </div>
+  );
 };
 
 export default App;
